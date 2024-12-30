1561be19c385150ac5c9d3f784af07386f527ab3
jeffevertt <jevertt@lwsd.org>
2024-12-30 15:10:57 -0800

    cleaned up the ML tank student default code

--- 14b_ML_tanks.py
+++ 14b_ML_tanks.py
@@ -2,11 +2,6 @@ from lib.window_arena import *
 from lib.winobj_tank import *
 from typing import List
 
-# update tank (this is where your code goes)
-def updateTank1(tank: Tank, powerUps: List[PowerUp], targets: List[Target]):
-    # TODO: once you have your ML model trained, switch the gameConfig to ADVANCED & hook it up here to drive the tank
-    pass
-
 # when in INTERACTIVE mode, use this to capture game state and commands & train your ML model
 def onExecCmd(cmd: TankCmd, levelTime: int, tank: Tank, powerUps: List[PowerUp], targets: List[Target]):
     # TODO: keep track of the data to be used during training
@@ -15,6 +10,11 @@ def onGameDone():
     # TODO: train your model
     pass
 
+
+# update tank (this is where your code goes once your ML model is trained)
+def updateTank1(tank: Tank, powerUps: List[PowerUp], targets: List[Target]):
+    # TODO: once you have your ML model trained, switch the gameConfig to ADVANCED & hook it up here to drive the tank
+    pass
 
 ####################################################################################################
 ############################## DO NOT MODIFY  METHODS BELOW THIS LINE ##############################
